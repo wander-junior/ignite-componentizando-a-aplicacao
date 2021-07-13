648c730960aa9ca3d858a11dd5c81db01ce234a5
Wander Júnior Cruz <wanderjrcruz96@gmail.com>
2021-07-13 18:12:07 -0300

    Implement Content component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useEffect, useState } from 'react';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
-
 import { SideBar } from './components/SideBar';
-// import { Content } from './components/Content';
+import { Content } from './components/Content';
 
 import { api } from './services/api';
 
@@ -61,19 +58,7 @@ export function App() {
   return (
     <div style={{ display: 'flex', flexDirection: 'row' }}>
       <SideBar genres={genres} handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} />
-      <div className="container">
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
+      <Content movies={movies} selectedGenre={selectedGenre}/>
     </div>
   )
 }
